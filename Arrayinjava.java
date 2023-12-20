f673db1186a82c68407f952d477e44142dd9cb89
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-20 13:02:17 +0530

    Code Completer for the Function

--- Arrayinjava.java
+++ Arrayinjava.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class Arrayinjava {
-    public static void update(int marks[]){
+    
+    public static void update(int marks[],int nonChangeble){
+        nonChangeble =10;
         for(int i=0; i<marks.length; i++){
             marks[i] = marks[i] + 1;
         }
@@ -9,7 +11,9 @@ public class Arrayinjava {
 
     public static void main(String args[]){
         int marks[]= {96, 97, 98};
-        update(marks);
+        int nonChangeble = 5;                        
+        update(marks,nonChangeble);
+        System.out.println(nonChangeble);
         //print array marks
         for(int i=0; i<marks.length; i++){
             System.out.println(marks[i]+" ");
