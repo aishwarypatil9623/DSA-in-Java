3f2687902c479b6bb545fb707541a9816ca4caef
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-20 12:20:00 +0530

    hti and trial

---
+++ Arrayinjava.java
@@ -0,0 +1,10 @@
+public class Arrayinjava {
+    public static void main(String args[]){
+        //creating an array
+        int marks[] = new int[50];
+
+        int numbers[]= {1,2,3};
+        int morenumbers[]= {4,5,6};
+        String Fruits[]= {"Apple","Mango","Pineapple"};
+    }
+}
