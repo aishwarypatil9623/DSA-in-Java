1af5583e5ceaad13a4e244a3f0e7833ad0322e20
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-21 21:36:13 +0530

    Prefix sum menthod for maximum sum

--- Arrayinjava.java
+++ Arrayinjava.java
@@ -1,19 +1,23 @@
 import java.util.*;
 
 public class Arrayinjava {
-    public static void maxsubarrays(int numbers[]){
+    public static void maxsubarrays(int numbers[]){          //Prifix sum 
         int currsum=0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];        
 
-        int maxSum=Integer.MIN_VALUE;
+        prefix[0] = numbers[0];
+        //calculate prefix sum 
+        for(int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
         for(int i=0;i<numbers.length; i++){
             int start=i;
-            for(int j=i;j<numbers.length; j++){
+            for(int j=i;j<numbers.length; j++){ 
                 int end = j;
-                currsum=0;
-                for(int k=start; k<=end; k++){         
-                    currsum+=numbers[k];
-                }
-                System.out.println(currsum);
+
+                currsum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+
                 if(maxSum<currsum){
                     maxSum=currsum;
                 }
