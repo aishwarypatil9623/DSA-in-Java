74cc53f38a86ee0f47862c06bab2f6376ec39b43
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-21 18:51:00 +0530

    Linear Search in Array

--- Arrayinjava.java
+++ Arrayinjava.java
@@ -1,23 +1,26 @@
 import java.util.*;
 
 public class Arrayinjava {
-    
-    public static void update(int marks[],int nonChangeble){
-        nonChangeble =10;
-        for(int i=0; i<marks.length; i++){
-            marks[i] = marks[i] + 1;
+    public static int linearsearch(int numbers[],int key){
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==key){
+                return i;
+            }
         }
-    }
 
-    public static void main(String args[]){
-        int marks[]= {96, 97, 98};
-        int nonChangeble = 5;                        
-        update(marks,nonChangeble);
-        System.out.println(nonChangeble);
-        //print array marks
-        for(int i=0; i<marks.length; i++){
-            System.out.println(marks[i]+" ");
-        }
-        System.out.println();
+        return -1;
     }
+    public static void main(String args[]){
+        int numbers[] = {2,4,6,8,10,12,14,16};
+        int key = 14;
+
+        int index = linearsearch(numbers, key);
+        if(index==-1){
+            System.out.println("Not Found");
+        }
+        else{
+            System.out.println("Key is at index: "+ index);
+        }
+
+   }
 }    
