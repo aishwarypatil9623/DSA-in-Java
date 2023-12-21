bfe936700b5149023a6327748223de9f67de6bce
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-22 04:37:09 +0530

    Kadanes max sum in Arrayinjava

--- Arrayinjava.java
+++ Arrayinjava.java
@@ -26,9 +26,27 @@ public class Arrayinjava {
 
         System.out.println("Max sum = "+ maxSum);
     }
+
+    public static void kadanes(int numbers[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i< numbers.length; i++){
+            cs= cs+numbers[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms = Math.max(cs,ms);
+        }
+
+        System.out.println("our max subarray sum is : " + ms);
+
+
+    }
     public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10,12,14,16};
+        int numbers[] = {2,4,-6,8,10,-22,14-16};
 
         maxsubarrays(numbers);
+        kadanes(numbers);
         }   
 }    
