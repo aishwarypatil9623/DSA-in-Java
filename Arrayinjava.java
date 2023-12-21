b99c03fc99e63275568b43262a224a9a3ff4eb24
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-21 19:06:06 +0530

    Smallest and Largest value in a Array

--- Arrayinjava.java
+++ Arrayinjava.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 public class Arrayinjava {
-    public static int linearsearch(int numbers[],int key){
-        for(int i=0; i<numbers.length; i++){
-            if(numbers[i]==key){
-                return i;
+    public static int getlargest(int numbers[]){
+        int largest=  Integer.MIN_VALUE;   //- Infinity
+        int smallest= Integer.MAX_VALUE;
+
+        for(int i =0; i<numbers.length; i++){
+            if (numbers[i]>largest) {
+                largest = numbers[i];
+            }
+            if (numbers[i]<smallest){
+                smallest = numbers[i];
             }
         }
-
-        return -1;
+        System.out.println("Smallest Value is "+smallest);
+        return largest;
     }
     public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10,12,14,16};
-        int key = 14;
-
-        int index = linearsearch(numbers, key);
-        if(index==-1){
-            System.out.println("Not Found");
+        int numbers[] = {2,4,6,8,10,32,14,16};
+        System.out.println("Largest Value is "+ getlargest(numbers));
         }
-        else{
-            System.out.println("Key is at index: "+ index);
-        }
-
-   }
 }    
