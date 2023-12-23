5ac7063a0b9c20a93c975e0dc0830b1789f3cf55
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-23 12:49:14 +0530

    Sorted Searche added

--- Sorting Algorithm/Matrix/Diagonalsum.java
+++ Sorting Algorithm/Matrix/Diagonalsum.java
@@ -28,13 +28,33 @@ public class Diagonalsum{
 
         return sum;
     }
+    public static boolean staircaseSearch(int matrix[][], int key) {
+        int row = 0,col = matrix[0].length-1;
+
+        while (row<matrix.length && col >= 0) {
+            if(matrix[row][col]==key){
+                System.out.println("found key at ("+row+","+col+")");
+                return true;
+            }
+            else if(key < matrix[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        System.out.println("Key not Found!");
+        return false;
+    }
 
     public static void main(String[] args){
-        int matrix[][] = {{1,2,3,4},
-                          {5,6,7,8},
-                          {9,10,11,12},
-                          {13,14,15,16}};
-    System.out.println(diagonalsum(matrix));
-    System.out.println(diagonalsum2(matrix));
+        int matrix[][] = {{10, 20, 30, 40},
+                          {15, 25, 35, 45},
+                          {27, 29, 37, 48},
+                          {32, 33, 39, 50}};
+        int key =33;
+        staircaseSearch(matrix, key);                 
+    //System.out.println(diagonalsum(matrix));
+    // System.out.println(diagonalsum2(matrix));
     }
 }
