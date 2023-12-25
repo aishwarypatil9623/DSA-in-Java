fec17452d1d949174f49a6e0d44c61e6d1c8b339
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-25 06:25:27 +0530

    Added LArgest Strings code in main

--- Strings.java/substring.java
+++ Strings.java/substring.java
@@ -12,5 +12,17 @@ public class substring {
     public static void main(String args[]) {
         String str = "Hello World";
         System.out.println(substring1(str,0,5));
+
+        String fruits[] = {"mango", "banana", "apple"};
+
+        String largest = fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+
     }
 }
