fef79a384b997f9edd4f7451556cb11c109eed8c
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-25 06:04:53 +0530

    my exception wala code

--- Strings.java/substring.java
+++ Strings.java/substring.java
@@ -1,3 +1,16 @@
+import java.util.*;
+
 public class substring {
-    
+    public static String substring1(String str,int si,int ei){
+        String substr = "";
+        for(int i=si; i<ei; i++){
+            substr +=substr.charAt(i);
+        }
+        return substr;
+    }
+
+    public static void main(String args[]) {
+        String str = "Hello World";
+        System.out.println(substring1(str,0,5));
+    }
 }
