4449cfb0b20ae9257a70e3efb4311916afd65da1
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2023-12-26 10:04:04 +0530

    Final code update

--- BitManipulation.java/getsetclearithbit.java
+++ BitManipulation.java/getsetclearithbit.java
@@ -30,11 +30,19 @@ public class getsetclearithbit{
         int bitMask = (~0)<<i;
         return n & bitMask;
     }
+
+    public static int clearrangebit(int n, int i, int j){
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
     public static void main(String[] args) {
         System.out.println(getIthBit(10, 2));
         System.out.println(setIthBit(10, 2));
         System.out.println(clearIthBit(10, 2));
         System.out.println(UpdateIthBit(10,2,0));
-        System.out.println(clearIthBit(10, 2));
+        System.out.println(clearlastIthBits(10, 2));
+        System.out.println(clearrangebit(10, 2, 7));
     }
 }
