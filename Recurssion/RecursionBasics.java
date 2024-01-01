147441e633a794497d59e643a25b162e1839ed15
aishwarypatil9623 <aishwaryspatil2003@gmail.com>
2024-01-01 05:30:28 +0530

    little changes

--- Recurssion/RecursionBasics.java
+++ Recurssion/RecursionBasics.java
@@ -36,11 +36,8 @@ public class RecursionBasics {
     }
 
     public static int Fibonaaci(int n){
-        if(n==0){
-            return 0;
-        }
-        else if(n==1){
-            return 1;
+        if(n==0 || n==1){
+            return n;
         }
         int fibnm1 = Fibonaaci(n-1);
         int fibnm2 = Fibonaaci(n-2);
