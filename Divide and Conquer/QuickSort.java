87f5a96bd7cce795e185a7ba13f4cfe8d52b773e
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-06 05:47:26 +0530

    Quick Sort Algo Code

--- Divide and Conquer/QuickSort.java
+++ Divide and Conquer/QuickSort.java
@@ -1,6 +1,47 @@
 public class QuickSort {
-    public static void quicksort
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int arr[], int si, int ei){
+        if(si>=ei){
+            return;
+        }
+        //last element
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1); //left part
+        quickSort(arr, pIdx+1, ei); //right part
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i =si-1;     //to make place for elements smaller than pivot
+
+        for(int j=si; j<ei; j++) {
+            if(arr[j] <= pivot) {
+                i++;
+                //swap
+                int temp = arr[j]; 
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }    
+        i++;
+        //swap
+        int temp = pivot; 
+        arr[ei] = arr[i];  //pivot = arr[i ]
+        arr[i] = temp;
+        return i;
+    }
+
     public static void main(String[] args) {
+        int arr[] = {1,9,5,8,4,2,6,7};
+        quickSort(arr, 0, arr.length-1);
+        printarr(arr);
+
         
     }
     
