e57b8a8875db28f51389e1a0bbb925fac0c09bb1
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-10 19:15:17 +0530

    Arraylist Declaration

---
+++ Array List /basics.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList; // or we may use java.util.*;
+
+public class basics {
+    public static void main(String[] args) {  // in java classes name start with capital letter ex. Integer,Boolean,etc
+        // arrayList is a part of JavaCollection FrameWork
+        //ClassName objectName  = new ClassName();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+    }
+}
