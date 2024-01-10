b236ebd8eb230a92f61690592e8cf5ac26f21582
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-10 19:19:19 +0530

    Added reverse order code

--- Array List /basics.java
+++ Array List /basics.java
@@ -5,7 +5,18 @@ public class basics {
         // arrayList is a part of JavaCollection FrameWork
         //ClassName objectName  = new ClassName();
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<String> list2 = new ArrayList<>();
-        ArrayList<Boolean> list3 = new ArrayList<>();
+        // ArrayList<String> list2 = new ArrayList<>();
+        // ArrayList<Boolean> list3 = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        //reverse print  - O(n) time complexity
+        for(int i=list.size()-1; i>=0; i--) {
+            System.out.println(list.get(i) + " ");
+        }
+        System.out.println();
     }
 }
