3b785f51b8aa9da8f99a4d6e98219638e6b08978
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-10 19:49:42 +0530

    Operators in ArrayList

--- Array List /basics.java
+++ Array List /basics.java
@@ -8,14 +8,49 @@ public class basics {
         // ArrayList<String> list2 = new ArrayList<>();
         // ArrayList<Boolean> list3 = new ArrayList<>();
         list.add(1);
-        list.add(2);
-        list.add(3);
+        list.add(2);  // Add Operator 
+        list.add(3);  //O(1)
         list.add(4);
         list.add(5);
+
+        list.add(1, 7);  //list.add(indexNo, elementToBeAdded at that index). //O(n)
 
         //reverse print  - O(n) time complexity
         for(int i=list.size()-1; i>=0; i--) {
             System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+        System.out.println(list);
+
+        //Get Operation - O(1)
+
+        int element = list.get(3); //add index name in the bracket 
+        System.out.println(element);
+
+        //Delete element - 
+
+        list.remove(2);
+        System.out.println(list);
+
+        //Set element at index
+
+        list.set(2,10);  //list.set(indexNo,elementToBeAdded)
+        System.out.println(list);
+
+        //Contains
+
+        System.err.println(list.contains(4));
+        System.out.println(list.contains(11));
+
+        //Size of an array
+
+        System.out.println(list.size());
+
+        //Print the arrayList
+
+        for(int i=0; i<=list.size(); i++) {
+            System.out.print(list.get(i)+" ");
         }
         System.out.println();
     }
