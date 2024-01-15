d669f2ac9c8043125580fc57229400643060d142
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-15 05:53:16 +0530

    Add first and add last operations

--- LinkedList/Linkelist.java
+++ LinkedList/Linkelist.java
@@ -14,8 +14,30 @@ public class Linkelist {
     public void addfirst(int data){
         //step1 = create new node
         Node newNode = new Node(data);
+
+        //step2 - newNode next = Head
+        newNode.next = head;   //link
+
+        //step3 - head = newNode
+        head = newNode;
+    }
+
+    public void addlast(int data){
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = tail;
+        tail = newNode;
     }
     public static void main(String[] args) {
+        Linkelist ll = new Linkelist();
+
+        ll.addfirst(2);
+        ll.addfirst(1);
+        ll.addlast(3);
+        ll.addlast(4);
         
     }
     
