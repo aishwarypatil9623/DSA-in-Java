df60342e1e95b612ade1e5bebe5272e03a097aa4
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-15 05:42:36 +0530

    .

---
+++ LinkedList/Linkelist.java
@@ -0,0 +1,22 @@
+public class Linkelist {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addfirst(int data){
+        //step1 = create new node
+        Node newNode = new Node(data);
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
