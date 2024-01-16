58ae965883e65fb55addf0005e9a4db9a8aff040
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-16 11:51:51 +0530

    added print Function

--- LinkedList/Linkelist.java
+++ LinkedList/Linkelist.java
@@ -3,42 +3,63 @@ public class Linkelist {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
 
-    public void addfirst(int data){
-        //step1 = create new node
+    public void addfirst(int data) {
+        // step1 = create new node
         Node newNode = new Node(data);
 
-        //step2 - newNode next = Head
-        newNode.next = head;   //link
+        // step2 - newNode next = Head
+        newNode.next = head;   // link
 
-        //step3 - head = newNode
+        // step3 - head = newNode
         head = newNode;
+        if (tail == null) {
+            tail = head; // Update tail if it is null
+        }
     }
 
-    public void addlast(int data){
+    public void addlast(int data) {
         Node newNode = new Node(data);
-        if(head == null) {
+        if (head == null) {
             head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is Empty");
             return;
         }
-        newNode.next = tail;
-        tail = newNode;
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
     }
+
     public static void main(String[] args) {
         Linkelist ll = new Linkelist();
 
+        ll.print();
         ll.addfirst(2);
+        ll.print();
         ll.addfirst(1);
+        ll.print();
         ll.addlast(3);
+        ll.print();
         ll.addlast(4);
-        
+        ll.print();
     }
-    
 }
