e363103a886cc0bf055a875f9bcd383d7d182008
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-18 16:00:09 +0530

    Deleting nth node from end

--- LinkedList/Linkelist.java
+++ LinkedList/Linkelist.java
@@ -157,6 +157,32 @@ public class Linkelist {
         head = prev;
     }
 
+    public void deleteNthNodefromEnd(int n){
+        int sz = 0;
+        Node temp = head;
+        while (temp != null){
+            temp = temp.next;
+            sz++;
+        }
+
+        if(n == sz) {  //for remoce first operation
+            head = head.next;
+            return;
+        }
+
+        //sz-n
+        int i=1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i < iToFind){
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+        return;
+    }
+
     public static void main(String[] args) {
         Linkelist ll = new Linkelist();
 
@@ -191,5 +217,7 @@ public class Linkelist {
         ll.reverse();
         ll.print();
 
+        ll.deleteNthNodefromEnd(3);
+        ll.print();
     }
 }
