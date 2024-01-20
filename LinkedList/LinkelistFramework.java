8e71c878287598db49ccc8ff914534186dd4a83c
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-21 02:14:22 +0530

    merge sort half committed

--- LinkedList/LinkelistFramework.java
+++ LinkedList/LinkelistFramework.java
@@ -17,8 +17,23 @@ public class LinkelistFramework {
     public static Node tail;
     public static int size;
 
-    public Node MergeSort(Node head){
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
 
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //mid node
+    }
+
+    private Node merge(Node head1, Node )
+
+    public Node MergeSort(Node head){
+        if(head == null && head.next == null) {
+            return head; 
+        }
         //find mid
         Node mid = getMid();
         //left & right MS
