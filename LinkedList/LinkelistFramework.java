1baf0c5ae5df5e04760b91ede014edc6697f4c16
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-01-23 15:11:32 +0530

    ,

--- LinkedList/LinkelistFramework.java
+++ LinkedList/LinkelistFramework.java
@@ -17,6 +17,19 @@ public class LinkelistFramework {
     public static Node tail;
     public static int size;
 
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
     private Node getMid(Node head) {
         Node slow = head;
         Node fast = head.next;
@@ -28,35 +41,77 @@ public class LinkelistFramework {
         return slow; //mid node
     }
 
-    private Node merge(Node head1, Node )
+    private Node merge(Node head1, Node head2) {
+        Node mergeDLL = new Node(-1);
+        Node temp = mergeDLL;
 
-    public Node MergeSort(Node head){
+        while (head1 != null && head2 != null){
+            if(head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;            
+        }
+
+        return mergeDLL.next;
+    }
+
+    public Node mergeSort(Node head){
         if(head == null && head.next == null) {
             return head; 
         }
         //find mid
-        Node mid = getMid();
+        Node mid = getMid(head);
         //left & right MS
         Node rightHead = mid.next;
         mid.next = null;
-        MergeSort(head);
-        MergeSort(rightHead);
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
 
         //merge
+        return merge(newLeft,newRight);
     }
     public static void main(String[] args) {
         //create - object int, float,boolean -> Integer, Float, Character
         LinkedList<Integer> ll = new LinkedList<>();
         //add
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addFirst(0);
-        // 0->1->2
-        System.out.println(ll);
-        //remove
-        ll.removeLast();
-        ll.removeFirst();
-        System.out.println(ll);
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addFirst(0);
+        // // 0->1->2
+        // System.out.println(ll);
+        // //remove
+        // ll.removeLast();
+        // ll.removeFirst();
+        // System.out.println(ll);
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        //5->4->3->2->1
+
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
+
     }
     
 
