35d4234088a531fe9ffeea684d166fbd1b19fa3c
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-02-06 07:34:57 +0530

    Reversing the stack that is output in same order as input

---
+++ Stack/reverseStack.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class reverseStack {
+    public static void pushatBottom( Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);  //adding our data when all the elements have been came out
+            return;
+        }
+        int top = s.pop();
+        pushatBottom(s, data); //recursion
+        s.push(top);
+    }    
+
+    public static void ReverseStack(Stack<Integer> s){
+        if(s.isEmpty()) {
+             return;   //since empty stack is always a reverse stack 
+        }
+        int top = s.pop();
+        ReverseStack(s);
+        pushatBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        //3,2,1
+        ReverseStack(s);
+        printStack(s);
+        //1,2,3
+    }
+}
