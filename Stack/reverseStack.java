1af921cb10c52702ff0716f8c759065cabb7ab2c
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2024-02-06 14:15:31 +0530

    uygfhugf

--- Stack/reverseStack.java
+++ Stack/reverseStack.java
@@ -17,7 +17,7 @@ public class reverseStack {
         }
         int top = s.pop();
         ReverseStack(s);
-        pushatBottom(s, top);
+        pushatBottom(s, top); // fyfyufufh
     }
 
     public static void printStack(Stack<Integer> s){
